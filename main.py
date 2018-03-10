b5ad0514e1eb5ed52778619bf564cfdff1dc628d
wthered <wthered@gmail.com>
2018-03-10 18:57:42 +0200

    Reading Movies Genres and general playing around

--- main.py
+++ main.py
@@ -1,39 +1,15 @@
-from datetime import datetime
-import calendar
-
-# with open("../ml-latest-small/genres.tmp.txt", "r") as f:
-#     for line in f:
-#         line = line[:-1]
-#         print("insert into movies.genres values (default,'" + line + "');")
-
-# movieId,title,genres
-# title_id | title | year | plot | poster
 movie = 0
 rating = 0.0
-d = datetime.utcnow()
-stamp = calendar.timegm(d.utctimetuple())
-
 output = open("genre.sql", "w")
 
-
-def fun(lorem):
-	line = lorem[:-1].split('"')
-	print(line)
-	return line
-
-
 for line in open("../ml-latest-small/movies.csv"):
-
-	(movie, title, genres) = line.split(",")
-
-	# genres = genres[:-1]
+	(movie, genres) = line.split(",")
 	for genre in genres[:-1].split('|'):
-		year = title[-5:-1]
-		title_q = "INSERT INTO movies.titles (title_id, title, year) VALUES (" + movie + ",'" + title + "'," + year + ");"
-		genre_q = "INSERT into movies.titles_genres SELECT " + movie + ", genre_id from movies.genres where genre = '" + genre + "';\n"
+		title_q = "INSERT INTO movies.titles (title_id) VALUES (" + movie + ");"
+		genre_q = "INSERT into movies.titles_genres SELECT " + movie + ", genre_id from movies.genres where genre = '" + genre + "';"
 		print(title_q)
 		print(genre_q)
 		output.write(title_q + "\n")
-		output.write(genre_q)
+		output.write(genre_q + "\n")
 
 output.close()
